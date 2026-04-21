bbbc906229214ad8eadb0aacc21a95c013af1ced
TheJarret084 <barrantesjarret2@gmail.com>
2026-04-20 18:50:56 -0600

    Actualizar AppConfig.hx

--- Android/androidBuild/app/src/android/AppConfig.hx
+++ Android/androidBuild/app/src/android/AppConfig.hx
@@ -10,9 +10,19 @@ class AppConfig {
     public static inline var PROCESSED_FOLDER:String = "processed";
     public static inline var EXPORTS_FOLDER:String = "exports";
     public static inline var SAVE_DIALOG_TITLE:String = "Guardar ZIP";
+
     // caros config
-    public static inline var CAROS_VIDEO_PATH:String = "assets/other/jejeje.mp4";
+    #if android
+    public static inline var CAROS_VIDEO_ASSET:String = "other/jejeje.mp4"; // nombre dentro de assets
+    #end
     public static inline var CAROS_VIDEO_DURATION_MS:Int = 16000;
     public static inline var CAROS_DIALOG_TITLE:String = "Error Fatal";
     public static inline var CAROS_DIALOG_MESSAGE:String = "Se detecto un problema critico. La app se cerrara.";
+
+    // Llama esto una vez que Lime ya inicio
+    #if android
+    public static function getCarosVideoPath():String {
+        return lime.system.System.applicationStorageDirectory + "/jejeje.mp4";
+    }
+    #end
 }
