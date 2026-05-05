c9681f482922d205e1ad77d66416dbecbe72c1b9
TheJarret084 <barrantesjarret2@gmail.com>
2026-05-04 20:35:03 -0600

    Actualizar index.js

--- page/index.js
+++ page/index.js
@@ -1,9 +1,10 @@
 const abelito = 'https://funkyatlas.abelitogamer.com';
-console.log("hola");
 
 const isMobile = /Android|iPhone|iPad|iPod/.test(navigator.userAgent);
 
-// Solo en desktop
+// Mostrar en la página si es mobile o no
+document.body.innerHTML += `<div style="position:fixed;top:0;left:0;background:red;color:white;padding:10px;z-index:9999;font-size:12px;">Mobile: ${isMobile}</div>`;
+
 if (!isMobile) {
   setInterval(() => {
     const start = performance.now();
