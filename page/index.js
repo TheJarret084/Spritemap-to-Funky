41b9b1039c94976b4cacd4d02f3745e4fa0180a7
TheJarret084 <barrantesjarret2@gmail.com>
2026-05-04 20:32:19 -0600

    Actualizar index.js

--- page/index.js
+++ page/index.js
@@ -3,7 +3,7 @@ console.log("hola");
 
 const isMobile = /Android|iPhone|iPad|iPod/.test(navigator.userAgent);
 
-// Solo en desktop, método 1
+// Solo en desktop
 if (!isMobile) {
   setInterval(() => {
     const start = performance.now();
@@ -13,27 +13,3 @@ if (!isMobile) {
     }
   }, 500);
 }
-
-// Método 2: desactivado (muy agresivo)
-// const detector = /./;
-// detector.toString = () => {
-//   window.location.replace(abelito);
-//   return '';
-// };
-// setInterval(() => {
-//   console.log(detector);
-//   console.clear();
-// }, 1000);
-
-// Método 3: resize más tolerante
-function checkDevTools() {
-  // Solo chequea si la diferencia es GRANDE (como si abres DevTools en desktop)
-  if (
-    window.outerWidth - window.innerWidth > 300 ||
-    window.outerHeight - window.innerHeight > 300
-  ) {
-    window.location.replace(abelito);
-  }
-}
-window.addEventListener('resize', checkDevTools);
-checkDevTools();
